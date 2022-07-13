4fb9c4d5723cec9c98f367afa4f1fef62330341b
dineinunes <nunesvieiraclaudinei@outlook.com>
2022-07-13 17:55:41 -0300

    feat: adds 'User Already Exists' error to middleware

--- back-end/src/middlewares/error.middleware.js
+++ back-end/src/middlewares/error.middleware.js
@@ -7,8 +7,14 @@ module.exports = function error(err, _req, res, _next) {
     return res.status(400).json({ message: err.message });
   }
 
+  if(err instanceof UserExists) {
+    return res.status(400).json({ message: err.message });
+  }
+
   return res.status(500).json({ message: 'Internal server error' });
 }
 
-class NotFound extends Error {};
 class IncorrectPassword extends Error {};
+class NotFound extends Error {};
+class UserExists extends Error {};
+
