448f940fdafc06a8f421ce15fc0b38b8a1e365e8
dineinunes <nunesvieiraclaudinei@outlook.com>
2022-07-13 16:22:31 -0300

    fix: adds bodyParser to app file

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const controllers = require('../controllers');
 const middlewares = require('../middlewares');
+
+app.use(bodyParser.json());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.post('/login', controllers.login);
