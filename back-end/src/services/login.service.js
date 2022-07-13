f5a48f6ec915046632ed6ae679a08fcf52727139
Natã Elienai <nata.elienai@gmail.com>
2022-07-12 21:12:38 -0300

    feat: add password validation and token creation to login service

--- back-end/src/services/login.service.js
+++ back-end/src/services/login.service.js
@@ -1,7 +1,8 @@
-// const crypto = require('crypto');
+const crypto = require('crypto');
+const jwt = require('../utils/jwt');
 const { User } = require('../database/models');
 
-async function login(email, _passwordPayload) {
+async function login(email, password) {
   const user = await User.find({
     where: {
       email,
@@ -12,7 +13,16 @@ async function login(email, _passwordPayload) {
     throw new Error('User not found');
   }
 
-  return user;
+  const hashedPassword = crypto.createHash('md5').update(password).digest("hex");
+
+  if (user.password !== hashedPassword) {
+    throw new Error('Incorrect password');
+  }
+
+  delete user.password;
+  const token = jwt.create(user);
+
+  return { ...user, token };
 }
 
 module.exports = {
