4a65ab24a9de33675cf233c7332057175a0772ee
dineinunes <nunesvieiraclaudinei@outlook.com>
2022-07-13 18:09:31 -0300

    fix: changes to correct function name

--- back-end/src/controllers/register.controller.js
+++ back-end/src/controllers/register.controller.js
@@ -3,7 +3,7 @@ const services = require('../services');
 module.exports = async function register(req, res, next) {
   try {
     const { name, email, password } = req.body;
-    const user = await services.login(name, email, password);
+    const user = await services.register(name, email, password);
     return res.status(201).json(user);
   } catch (error) {
     next(error);
