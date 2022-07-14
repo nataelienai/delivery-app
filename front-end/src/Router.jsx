d6ab688d0f039673d27542e3c26f546a4bb52ce3
Lucas Duarte <lucs.rkt@gmail.com>
2022-07-13 22:39:11 -0300

    feat: change route to /customer/products

--- front-end/src/Router.jsx
+++ front-end/src/Router.jsx
@@ -10,7 +10,7 @@ export default function Router() {
       <Route exact path="/" element={ <Navigate to="/login" /> } />
       <Route exact path="/login" element={ <Login /> } />
       <Route exact path="/register" element={ <Register /> } />
-      <Route exact path="/customer" element={ <Customer /> } />
+      <Route exact path="/customer/products" element={ <Customer /> } />
     </Routes>
   );
 }
