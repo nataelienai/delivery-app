3da869799aa6fd5b4765161fc8df87dd57e0dad2
Lucas Duarte <lucs.rkt@gmail.com>
2022-07-14 02:09:23 -0300

    feat: add field to show order total value

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -4,8 +4,14 @@ import CheckoutTable from '../components/CheckoutTable';
 export default function Checkout() {
   return (
     <main>
-      oi Checkout
-      <CheckoutTable />
+      <div>
+        <CheckoutTable />
+        <div
+          data-testid="customer_checkout__element-order-total-price"
+        >
+          soma dos produtos do checkout
+        </div>
+      </div>
     </main>
   );
 }
