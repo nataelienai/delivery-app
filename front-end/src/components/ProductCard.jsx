0c73728efc0c9b31af1ab46a182263f978378692
Lucas Duarte <lucs.rkt@gmail.com>
2022-07-13 22:15:01 -0300

    feat: add proptypes in products card

--- front-end/src/components/ProductCard.jsx
+++ front-end/src/components/ProductCard.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import P from 'prop-types';
 
-export default function ProductCard() {
+export default function ProductCard({ product, id }) {
   return (
     <div>
-      <h2 data-testid="customer_products__element-card-title-<id>">product name</h2>
-      <h2 data-testid="customer_products__element-card-title-<id>">product price</h2>
+      <h2
+        data-testid={ `customer_products__element-card-title-${id}` }
+      >
+        { product.name }
+      </h2>
+      <h2
+        data-testid={ `customer_products__element-card-title-${id}` }
+      >
+        { product.price }
+      </h2>
       <div data-testid="customer_products__img-card-bg-image-<id>">product image</div>
       <div>
         <button
@@ -24,3 +33,11 @@ export default function ProductCard() {
     </div>
   );
 }
+
+ProductCard.propTypes = {
+  product: P.shape({
+    name: P.string,
+    price: P.number,
+  }).isRequired,
+  id: P.number.isRequired,
+};
