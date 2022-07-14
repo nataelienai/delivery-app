3c63529ecfa277a35c3548f696eed790b304a62b
Lucas Duarte <lucs.rkt@gmail.com>
2022-07-14 01:57:06 -0300

    feat: add data-testid in items

--- front-end/src/components/CheckoutTableRows.jsx
+++ front-end/src/components/CheckoutTableRows.jsx
@@ -4,12 +4,36 @@ import P from 'prop-types';
 export default function CheckoutTableRow({ item, id }) {
   return (
     <tr>
-      <td>{id}</td>
-      <td>{item.name}</td>
-      <td>{item.quantity}</td>
-      <td>{item.unitValue}</td>
-      <td>{item.quantity * item.unitValue}</td>
-      <td>Remover</td>
+      <td
+        data-testid={ `customer_checkout__element-order-table-item-number-${index}` }
+      >
+        {id + 1}
+      </td>
+      <td
+        data-testid={ `customer_checkout__element-order-table-name-${index}` }
+      >
+        {item.name}
+      </td>
+      <td
+        data-testid={ `customer_checkout__element-order-table-quantity-${index}` }
+      >
+        {item.quantity}
+      </td>
+      <td
+        data-testid={ `customer_checkout__element-order-table-unit-price-${index}` }
+      >
+        {item.unitValue}
+      </td>
+      <td
+        data-testid={ `customer_checkout__element-order-table-sub-total-${index}` }
+      >
+        {item.quantity * item.unitValue}
+      </td>
+      <td
+        data-testid={ `customer_checkout__element-order-table-remove-${index}` }
+      >
+        Remover
+      </td>
     </tr>
   );
 }
