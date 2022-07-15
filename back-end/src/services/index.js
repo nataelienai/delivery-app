ef4ba564c0782869810e2053391996cef796e54d
Natã Elienai <nata.elienai@gmail.com>
2022-07-15 17:50:57 -0300

    feat: create the getByUserId function in sale service

--- back-end/src/services/index.js
+++ back-end/src/services/index.js
@@ -1,9 +1,11 @@
 const login = require('./login.service');
 const register = require('./register.service');
 const getAll = require('./product.service');
+const sale = require('./sale.service');
 
 module.exports = {
   login,
   register,
   getAll,
+  sale,
 };
---
+++ back-end/src/services/sale.service.js
@@ -0,0 +1,26 @@
+const sequelize = require('sequelize');
+const { Sale, User, Product } = require('../database/models');
+
+module.exports = {
+  async getByUserId(userId) {
+    return Sale.findAll({
+      where: { userId },
+      include: [
+        { model: User, as: 'seller', attributes: { exclude: ['password'] } },
+        {
+          model: Product,
+          as: 'products',
+          attributes: [
+            'id',
+            'name',
+            'price',
+            'urlImage',
+            [sequelize.literal('`products->SaleProduct`.quantity'), 'quantity'],
+          ],
+          through: { attributes: [] },
+        },
+      ],
+      attributes: { exclude: ['userId', 'sellerId'] }
+    });
+  },
+};
