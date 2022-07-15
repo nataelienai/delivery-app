5231e6db9e0418bd7412da82546e285505b7b92d
Lucas Duarte <lucs.rkt@gmail.com>
2022-07-14 21:45:01 -0300

    refactor: componentize Products

--- front-end/src/pages/Customer.jsx
+++ front-end/src/pages/Customer.jsx
@@ -1,23 +1,15 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import Header from '../components/Header';
-import ProductCard from '../components/ProductCard';
-import CustomerProvider, { customerContext } from '../context/CustomerProvides';
+import Products from '../components/Products';
+import CustomerProvider from '../context/CustomerProvides';
 
 export default function Customer() {
-  const { products } = useContext(customerContext);
-
   return (
-    <CustomerProvider>
-      <main>
-        <Header />
-        <div>
-          {
-            products
-            && products.map((product, index) => (
-              <ProductCard product={ product } key={ index } id={ product.id } />))
-          }
-        </div>
-      </main>
-    </CustomerProvider>
+    <main>
+      <Header />
+      <CustomerProvider>
+        <Products />
+      </CustomerProvider>
+    </main>
   );
 }
