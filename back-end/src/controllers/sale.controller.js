efa0308de0c4968d651b4b6dd43aa05707823bd6
dineinunes <nunesvieiraclaudinei@outlook.com>
2022-07-15 16:42:16 -0300

    fix: fixes to correct function name

--- back-end/src/controllers/sale.controller.js
+++ back-end/src/controllers/sale.controller.js
@@ -3,7 +3,7 @@ const services = require('../services');
 module.exports = async function getSaleById(req, res, next) {
   try {
     const { id } = req.params;
-    const user = await services.login(id);
+    const user = await services.getSaleById(id);
     return res.status(200).json(user);
   } catch (error) {
     next(error);
@@ -13,7 +13,7 @@ module.exports = async function getSaleById(req, res, next) {
 module.exports = async function updateSaleStatus(req, res, next) {
   try {
     const { id, status } = req.params;
-    const user = await services.login(id, status);
+    const user = await services.updateSaleStatus(id, status);
     return res.status(200).json(user);
   } catch (error) {
     next(error);
