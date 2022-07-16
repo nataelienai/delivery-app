8bb0c888e70322e162b69776def3e40de9fb5ae7
Natã Elienai <nata.elienai@gmail.com>
2022-07-16 17:56:03 -0300

    fix: remove useless fields from get sales by user response

--- back-end/src/services/sale.service.js
+++ back-end/src/services/sale.service.js
@@ -1,23 +1,12 @@
-const sequelize = require('sequelize');
-const { Sale, User, Product } = require('../database/models');
+const { Sale } = require('../database/models');
 
 module.exports = {
   async getByUserId(userId) {
     return Sale.findAll({
       where: { userId },
-      include: [
-        { model: User, as: 'seller', attributes: { exclude: ['password'] } },
-        {
-          model: Product,
-          as: 'products',
-          attributes: ['id', 'name', 'price', 'urlImage', [
-            sequelize.literal('`products->SaleProduct`.quantity'),
-            'quantity',
-          ]],
-          through: { attributes: [] },
-        },
-      ],
-      attributes: { exclude: ['userId', 'sellerId'] },
+      attributes: {
+        exclude: ['userId', 'sellerId', 'deliveryAddress', 'deliveryNumber']
+      },
     });
   },
 };
