286bb98e0654a83c7eff17adb39bd9b0c7e59548
Lucas Duarte <lucs.rkt@gmail.com>
2022-07-16 14:33:46 -0300

    feat: add total order template

---
+++ front-end/src/components/OrderTotal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function OrderTotal() {
+  return (
+    <div
+      data-testid="customer_order_details__element-order-total-price"
+    >
+      total do pedido
+    </div>
+  );
+}
--- front-end/src/pages/Order.jsx
+++ front-end/src/pages/Order.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../components/Header';
 import OrderLabel from '../components/OrderLabel';
+import OrderTable from '../components/OrderTable';
+import OrderTotal from '../components/OrderTotal';
 
 export default function Order() {
   return (
     <main>
       <Header />
       <OrderLabel />
+      <OrderTable />
+      <OrderTotal />
     </main>
   );
 }
