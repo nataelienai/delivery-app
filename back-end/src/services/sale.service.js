0f7fe831583ceec82438ea4abea0f59ae5abb294
Natã Elienai <nata.elienai@gmail.com>
2022-07-16 17:46:45 -0300

    feat: add required fields to getSaleById function return

--- back-end/src/services/sale.service.js
+++ back-end/src/services/sale.service.js
@@ -1,11 +1,23 @@
-const { Sale } = require('../database/models');
+const sequelize = require('sequelize');
+const { Sale, User, Product } = require('../database/models');
 
 module.exports = {
   async getSaleById(id) {
     return Sale.findOne({
-      where: {
-        id,
-      },
+      where: { id },
+      include: [
+        { model: User, as: 'seller', attributes: ['name'] },
+        {
+          model: Product,
+          as: 'products',
+          attributes: ['id', 'name', 'price', [
+            sequelize.literal('`products->SaleProduct`.quantity'),
+            'quantity',
+          ]],
+          through: { attributes: [] },
+        },
+      ],
+      attributes: { exclude: ['userId', 'sellerId', 'deliveryAddress', 'deliveryNumber'] },
     });
-  }
+  },
 };
