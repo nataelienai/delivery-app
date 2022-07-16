1a6e8d03aed4aab7170d854836727f8691e50e23
Lucas Duarte <lucs.rkt@gmail.com>
2022-07-16 16:13:56 -0300

    chore(CustomerProvider): change module name

--- front-end/src/pages/Customer.jsx
+++ front-end/src/pages/Customer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Products from '../components/Products';
-import CustomerProvider from '../context/CustomerProvides';
+import CustomerProvider from '../context/CustomerProvider';
 
 export default function Customer() {
   return (
