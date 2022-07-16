b5b3cb2ccd4864233fd823b806f315e1a347c819
Lucas Duarte <lucs.rkt@gmail.com>
2022-07-16 16:03:12 -0300

    feat: add ordercard in orders page

--- front-end/src/pages/Orders.jsx
+++ front-end/src/pages/Orders.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import OrderCard from '../components/OrderCard';
 
 export default function Orders() {
   return (
     <main>
       <Header />
+      <div>
+        { Array.from({ length: 3 }, () => <OrderCard />)}
+      </div>
     </main>
   );
 }
