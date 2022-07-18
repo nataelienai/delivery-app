aaded0d0e38aa56fe2e45bcbe320719266ff4d6e
Calixto-Wellington <wellington1081@gmail.com>
2022-07-18 18:06:03 -0300

    feat: add the sales creation endpoint

--- back-end/src/controllers/sale.controller.js
+++ back-end/src/controllers/sale.controller.js
@@ -2,8 +2,8 @@ const { saleService } = require('../services');
 
 module.exports = async function sale(req, res, next ) {
   try {
-    const { userId, sellerId, totalPrice, deliveryAddress, deliveryNumber } = req.body
-    const newSale = await saleService({userId, sellerId, totalPrice, deliveryAddress, deliveryNumber})
+    const { userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, products } = req.body
+    const newSale = await saleService({userId, sellerId, totalPrice, deliveryAddress, products, deliveryNumber})
     return res.status(201).json(newSale);
   }catch (error) {
     console.log(error);
--- back-end/src/services/sale.service.js
+++ back-end/src/services/sale.service.js
@@ -1,17 +1,24 @@
-const { Sale } = require('../database/models');
+const { Sale, SaleProduct } = require('../database/models');
 
-module.exports = async function SaleService (sale) {
-    
-    const { userId, sellerId, totalPrice, deliveryAddress, deliveryNumber} =  sale ;
-    newSale = await Sale.create({
-      userId,
-      sellerId,
-      totalPrice,
-      deliveryAddress,
-      deliveryNumber,
-      saleDate: new Date(),
-      status: 'Pendente'
+module.exports = async function SaleService(sale) {
+  const { userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, products } = sale;
+  
+  const newSale = await Sale.create({
+    userId,
+    sellerId,
+    totalPrice,
+    deliveryAddress,
+    deliveryNumber,
+    saleDate: new Date(),
+    status: 'Pendente',
+  });
+
+  await Promise.all(products.map(async (product) => {
+    SaleProduct.create({
+        saleId: newSale.id,
+        productId: product.id,
+        quantity: product.quantity,
     });
-
-    return newSale;
-}
+  }));
+  return newSale;
+};
