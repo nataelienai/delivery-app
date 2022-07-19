ef346d4737cebf1617711137cafbe061eb82424d
Lucas Duarte <lucs.rkt@gmail.com>
2022-07-18 22:19:06 -0300

    feat: add totalPrice real value

--- front-end/src/components/OrderTotal.jsx
+++ front-end/src/components/OrderTotal.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import P from 'prop-types';
 
-export default function OrderTotal() {
+export default function OrderTotal({ totalPrice }) {
   return (
     <div
       data-testid="customer_order_details__element-order-total-price"
     >
-      total do pedido
+      { totalPrice.replace(/\./, ',') }
     </div>
   );
 }
+
+OrderTotal.propTypes = {
+  totalPrice: P.string.isRequired,
+};
