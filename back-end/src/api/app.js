fd9afa7d890818586719ef5cdd453e54336b7671
Natã Elienai <nata.elienai@gmail.com>
2022-07-21 21:00:38 -0300

    refactor: replace bodyparser's json parser with express's

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 
 const app = express();
 
 const controllers = require('../controllers');
 const middlewares = require('../middlewares');
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 app.use(express.static('public'));
 
